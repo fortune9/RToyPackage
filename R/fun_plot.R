0a0c5cafeac11f84af928afc44a50588d2b25cc3
Zhenguo Zhang <fortune9@users.noreply.github.com>
2020-08-13 12:37:03 -0700

    added help to fun_plot() function

--- R/fun_plot.R
+++ R/fun_plot.R
@@ -1,3 +1,17 @@
+#' plot sin(x) and sin(x/2) functions
+#'
+#' calling curve() to plot these functions.
+#'
+#' @param from the starting point for x-axis
+#' @param to  the ending point for x-axis
+#' @param ... other parameters directly passed to curve()
+#'
+#' @return Null
+#' @export
+#'
+#' @examples
+#' fun_plot(-pi, pi)
+#'
 fun_plot <- function(from = -pi, to = pi, ...) {
   curve(sin, from = from, to = to, ylab="y",  ...)
   curve(sin(x / 2), from = from, to = to, add = T, col = "red")
